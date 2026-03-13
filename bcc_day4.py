68efb4e473fd15042cbe6d022e8651c0ea0b0184
Onkar0726 <olondhe726@gmail.com>
2026-03-13 11:53:38 +0530

    day 3

---
+++ bcc_day4.py
@@ -0,0 +1,60 @@
+# i=1
+# while i<=5:
+#     print(i)
+#     i=i+1
+
+# username=''
+# password=''
+# while username != "admin" and password !="hello":
+#     username = input("enter username :")
+#     password = input("enter password :")
+
+
+# n=int(input("enter numbers :"))
+# sum=0
+# i=1
+# while i<=n :
+#     sum=sum+i
+#     i=i+1
+# print(" the sum of first",n,"number is  :",sum)   
+  
+
+# name="Omkar"
+# newname=""
+
+# for i in name:
+#     if i not in newname:
+#         newname+=i
+# print(name) 
+# print(newname)       
+
+#WAP to reverse the string using any loop
+# name="om"
+# print(name[::-1])
+
+
+# mycart=[10,20,200,300,800,60,700]
+# for i in mycart:
+#     if i>400:
+#         print("this is my purchased item")
+#         continue
+#     print(i)
+
+
+#WAP to check if given string is palindrome
+# name= "racecar"
+# if name==name[::-1]:
+#     print("palindrome")
+# else:
+#     print("not palindrome")    
+
+# #wap to check anagrams
+# name="londhe"
+# for i in name
+#@ wap nested for loop #111 222 333 
+
+n=int(input("enter num"))
+for i in range (1,n+1):
+    for j in range (1,n+1):
+        print(n+1-i,end=" ")
+    print()
