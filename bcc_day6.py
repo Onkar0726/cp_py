c3942dcf03b1cdd22f24eb5c50da5ec7821cf972
Onkar0726 <olondhe726@gmail.com>
2026-03-17 11:12:18 +0530

    day 6

---
+++ bcc_day6.py
@@ -0,0 +1,55 @@
+# fruit_list1 = ['Apple','Berry','Papaya']
+# fruit_list2 = fruit_list1
+# fruit_list3 = fruit_list1[:]
+
+# fruit_list2[0] = 'Guava'
+# fruit_list3[1] = 'kiwi'
+
+# sum = 0
+# for ls in (fruit_list1, fruit_list2, fruit_list3):
+#     if ls[0] == 'Guava':
+#         sum += 1
+#     if ls[1] == 'kiwi':
+#         sum += 20
+
+# print(sum)
+
+
+
+# def f(i,value = []):
+#     value.append(i)
+#     print (value)
+# f(1)    
+# f(2)
+# f(3)
+
+
+
+# def func(value, values): 
+#     var = 1
+#     values[0] = 44
+# t = 3
+# v = [1,2,3]
+# func(t,v)
+# print(t, v[0])
+
+
+
+# dict = {'c': 97, 'a':96, 'b': 98}
+# for _ in sorted(dict):
+#     print(dict[_])
+
+
+
+# fruit = {}
+# def addone(index):
+#     if index in fruit:
+#         fruit[index] += 1
+#     else:
+#         fruit[index] = 1
+# addone('Apple')
+# addone('Banana')
+# addone('apple')
+# print(len(fruit))
+
+#given an array return an array where  each element is  the product of all the element in the array except itself
